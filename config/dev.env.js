494f49c01134eb0c9cf1d5b4d1b86e04b4289852
zhangrui <zhangruiangela@qq.com>
2018-08-09 14:50:43 +0800

    test

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,5 +5,5 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   ENV_CONFIG: '"dev"',
-  BASE_API: '"166.111.83.93:8199"'
+  BASE_API: '"166.111.83.83:8199"'
 })
